2b6b0e6cd32b7585cfc3990f699a1da2cdcd1023
Rohit Borse <codingwithrb@gmail.com>
2023-04-28 18:31:47 +0530

    Profile session update

--- app/User/views.py
+++ app/User/views.py
@@ -4,6 +4,7 @@ from app.db import mysql
 user = Blueprint("User", __name__, url_prefix="/user",
                  template_folder="templates")
 
+profileHealth  = 10    
 
 @user.route('/')
 def home():
@@ -13,7 +14,7 @@ def home():
     cursor.execute('SELECT * FROM users WHERE fname= %s ', [username])
 
     user = cursor.fetchone()
-    return render_template("profile.html", username=username, user=user)
+    return render_template("profile.html", username=username, user=user, ph=profileHealth)
 
 
 @user.route('/event_progress')
