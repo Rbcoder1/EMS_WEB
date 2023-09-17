235f54670c8dd2447569ef50b164a6ee0715fd6d
Rohit Borse <codingwithrb@gmail.com>
2023-09-17 19:54:50 +0530

    admin pannel added

--- app/Admin/views.py
+++ app/Admin/views.py
@@ -1,8 +1,24 @@
 from flask import Blueprint, render_template
 
-admin = Blueprint("Admin", __name__,url_prefix="/admin")
-
+admin = Blueprint("Admin", __name__,url_prefix="/admin",template_folder="./templates")
 
 @admin.route('/')
 def home():
-    return "Admin"
+    return render_template('index.html')
+
+@admin.route('/student')
+def student():
+    return render_template('student.html')
+
+@admin.route('/dash')
+def dash():
+    return render_template('dash.html')
+
+@admin.route('/hackethon')
+def hackethon():
+    return render_template('hackethon.html')
+
+@admin.route('/filed')
+def filed_work(): 
+    return render_template('filed_work.html')
+
